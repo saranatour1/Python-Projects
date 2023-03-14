d909039cb4ba661f9ca03f22ee140953b6e6209d
Sara <saranatourcontact@gmail.com>
2023-03-14 17:21:00 +0200

    Time Display assignment

---
+++ django/django_fundamentals/Day 23/Code Folder/1- Time Display/Time_display/Time_display/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from time import gmtime, strftime,localtime
+def display_time(request):
+    context = {
+        "time": strftime("%H:%M:%S %p", localtime()),
+        "date":strftime("%Y-%m-%d",localtime())
+        
+    }
+    print(context)
+    return render(request,'index.html', context)
+
