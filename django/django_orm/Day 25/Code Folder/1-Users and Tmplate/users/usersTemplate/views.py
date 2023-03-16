8ab5b99a47e3b6d0d349c904e11af10a92f6f208
Sara <saranatourcontact@gmail.com>
2023-03-17 00:36:25 +0200

    added scroll bar to the table, and normailize

--- django/django_orm/Day 25/Code Folder/1-Users and Tmplate/users/usersTemplate/views.py
+++ django/django_orm/Day 25/Code Folder/1-Users and Tmplate/users/usersTemplate/views.py
@@ -4,7 +4,10 @@ from .models import User
 
 def index(request):
   if request.method == 'POST':
-    User.objects.create(first_name=request.POST['first_name'],last_name=request.POST['last_name'],email_address=request.POST['email'],age=request.POST['age'])
+    User.objects.create(first_name=request.POST['first_name']
+                        ,last_name=request.POST['last_name'],
+                        email_address=request.POST['email'],
+                        age=request.POST['age'])
   
   context = {
     "all_the_users": User.objects.all()
