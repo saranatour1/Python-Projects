6200d49400fd7951596cbea640ef1325422a200d
Sara <saranatourcontact@gmail.com>
2023-03-16 17:25:33 +0200

    users and templates

---
+++ django/django_orm/Day 25/Code Folder/0-Users(shell)/users/usersTemplate/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UserstemplateConfig(AppConfig):
+    name = 'usersTemplate'
---
+++ django/django_orm/Day 25/Code Folder/0-Users(shell)/users/usersTemplate/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render,HttpResponse
+from .models import User
+
+
+def index(request):
+  if request.method == 'POST':
+    User.objects.create(first_name=request.POST['first_name'],last_name=request.POST['last_name'],email_address=request.POST['email'],age=request.POST['age'])
+  
+  context = {
+    "all_the_users": User.objects.all()
+  }
+  return render(request,"index.html",context)
+# Create your views here.
+
+
+
+
