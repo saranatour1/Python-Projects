d3d08d344bd2de37b55469b3ed40e3397539b91f
Sara <saranatourcontact@gmail.com>
2023-03-17 23:19:12 +0200

    fixing some issues with colors, and reduced the code, missing comments

--- django/django_fundamentals/Day 24/2-Ninja Gold/dojo_gold/dojo_gold/settings.py
+++ django/django_fundamentals/Day 24/2-Ninja Gold/dojo_gold/dojo_gold/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'dojo_gold',
+   
     'gold_game',
     'django.contrib.admin',
     'django.contrib.auth',
--- django/django_fundamentals/Day 24/2-Ninja Gold/dojo_gold/gold_game/views.py
+++ django/django_fundamentals/Day 24/2-Ninja Gold/dojo_gold/gold_game/views.py
@@ -1,51 +1,52 @@
+#import the needed libraries, like render redirect , random and time
 from django.shortcuts import render, HttpResponse, redirect
-import random  # import the random module
+import random  
 from time import gmtime, strftime,localtime
 
+# the main method 
+
 def index(request):
-  count = request.session.get('count', 0)
-  request_activity=request.session.get('request_activity',0)
-  time =request.session.get('time',0)
-  date=request.session.get('date',0)
-  proccessed=request.session.get('proccessed')
-  activities = request.session.get('activities', [])
-  random_oparator=request.session.get('random_oparator')
-  context = {'count': count,'request_activity':request_activity, 'time':time, 'date':date,'proccessed':proccessed ,'activities':activities,'random_oparator':random_oparator }  
+  context = {'count': request.session.get('count', 0),
+            'activities':request.session.get('activities', []),
+            }  
   return render(request, 'index.html', context)
 
 def process_money(request):
-  request.session['proccessed']=False
   x = 0
-  random_operator = 0
   if request.method == 'POST':
-    form_name = request.POST['which_form']
-    request.session['proccessed']=True
-    x = random.randint(10, 20)
-    random_operator = random.randint(0, 1)
-    request.session['random_oparator']=random_operator
-    quest_random_int= random.randint(0, 50)
-    current_count=request.session.get('count', 0)
-    request.session['request_activity']=x
-    if request.POST.get('which_form') == 'farm':
-      request.session['count'] = current_count + x
-    elif request.POST.get('which_form') == 'cave':
-      request.session['count'] = current_count + x
-    elif request.POST.get('which_form') == 'house':
-      request.session['count'] = current_count + x
-    elif request.POST.get('which_form') == 'quest':
-      if random_operator == 0:
-        request.session['count'] = current_count -quest_random_int
-        request.session['request_activity']=quest_random_int
+      form_name = request.POST['which_form']
+      x = random.randint(10, 20)
+      quest_random_int = random.randint(-50, 50)
+      current_count = request.session.get('count', 0)
+      request.session['request_activity'] = x
+      
+      if form_name in ['farm', 'cave', 'house']:
+          request.session['count'] = current_count + x
+      elif form_name == 'quest':
+          request.session['count'] = current_count + quest_random_int
+          request.session['request_activity'] = quest_random_int
+          
+      request.session['time'] = strftime("%I:%M:%S %p", localtime())
+      request.session['date'] = strftime("%B %d %Y", localtime())
+      
+      if form_name == 'quest':
+          if quest_random_int < 0:
+              activity_str = (f'<p class="red">'
+                              f'You entered a {form_name} and lost {-quest_random_int} golds on '
+                              f'{request.session.get("date")} {request.session.get("time")}</p>')
+          else:
+              activity_str = (f'<p class="green">'
+                              f'You entered a {form_name} and got {quest_random_int} golds on '
+                              f'{request.session.get("date")} {request.session.get("time")}</p>')
       else:
-        request.session['count'] = current_count +quest_random_int
-        request.session['request_activity']=quest_random_int
+          activity_str = (f'<p class="green">'
+                          f'You entered a {form_name} and got {x} golds on '
+                          f'{request.session.get("date")} {request.session.get("time")}</p>')
+          
+      activities = request.session.get('activities', [])
+      activities.append(activity_str)
+      request.session['activities'] = activities
+      
+      return redirect('/')
 
-    request.session['time']=strftime("%I:%M:%S %p", localtime())
-    request.session['date']= strftime("%B %d %Y", localtime())
-    print(request.session['time'],request.session['date'])
-    activity_str = (f"You entered a {form_name} and got {x} golds on ({request.session.get('date')} {request.session.get('time')})")
-    activities = request.session.get('activities', [])
-    activities.append(activity_str)
-    request.session['activities'] = activities
-    
-    return redirect('/')
+
