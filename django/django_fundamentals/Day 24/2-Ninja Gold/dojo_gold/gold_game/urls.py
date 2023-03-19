dbd5a2b05b492215673af0ebdf2454149fc2792d
Sara <saranatourcontact@gmail.com>
2023-03-19 15:46:20 +0200

    more edits on session

--- django/django_fundamentals/Day 24/2-Ninja Gold/dojo_gold/gold_game/urls.py
+++ django/django_fundamentals/Day 24/2-Ninja Gold/dojo_gold/gold_game/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('', views.index),
-    path('process_money',views.process_money)
+    path('process_money',views.process_money),
+    path('destroy_session',views.clear_session)
 ]
--- django/django_fundamentals/Day 24/2-Ninja Gold/dojo_gold/gold_game/views.py
+++ django/django_fundamentals/Day 24/2-Ninja Gold/dojo_gold/gold_game/views.py
@@ -18,11 +18,12 @@ def process_money(request):
         x = random.randint(10, 20)
         quest_random_int = random.randint(-50, 50)
         request.session['request_activity'] = x
+        current_count = request.session.get('count', 0)
         
         if form_name in ['farm', 'cave', 'house']:
-            request.session['count'] += x
+            request.session['count'] = current_count + x
         elif form_name == 'quest':
-            request.session['count'] += quest_random_int
+            request.session['count'] = current_count + quest_random_int
             request.session['request_activity'] = quest_random_int
         
         request.session['time'] = strftime("%I:%M:%S %p", localtime())
@@ -42,11 +43,12 @@ def process_money(request):
                             f'You entered a {form_name} and got {x} golds on '
                             f'{request.session.get("date")} {request.session.get("time")}</p>')
         
-        activities = request.session.get('activities', [])
-        activities.append(activity_str)
+        activities = request.session.get('activities') or []
+        activities.insert(0, activity_str)
         request.session['activities'] = activities
         
         return redirect('/')
 
-
-
+def clear_session(request):
+    request.session.clear()
+    return redirect('/')
