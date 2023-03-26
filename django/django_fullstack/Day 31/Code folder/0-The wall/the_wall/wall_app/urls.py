13c1dd5ff8395855870c059b304fd405434dede7
Sara <saranatourcontact@gmail.com>
2023-03-26 14:14:26 +0300

    html files

--- django/django_fullstack/Day 31/Code folder/0-The wall/the_wall/wall_app/urls.py
+++ django/django_fullstack/Day 31/Code folder/0-The wall/the_wall/wall_app/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('',views.main_page),
+    path('add_new_message',views.add_message),
+    path('wall/',views.viewWall),
+    path('add_new_comment',views.add_comment)
+    
 
 ]
--- django/django_fullstack/Day 31/Code folder/0-The wall/the_wall/wall_app/views.py
+++ django/django_fullstack/Day 31/Code folder/0-The wall/the_wall/wall_app/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect 
+from wall_app.models import * 
+from login.models import *
 
 # Create your views here.
 
+# the main page to display all the messages,allow users to post messages as well. 
 def main_page(request):
-  return HttpResponse("hello")
+  return render(request,"index.html")
+
+
+# Handle adding messages direction
+def add_message(request):
+  if request.metnod=='POST':
+    Messages.objects.create()
+  return redirect('/')
+
+#views the page of the wall , where users can add comments and messages
+def viewWall(request):
+  return render(request,"thewall.html")
+
+
+#handle adding a comment
+def add_comment(request):
+  return redirect('/wall')
